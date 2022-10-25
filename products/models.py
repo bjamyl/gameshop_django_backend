a7e7f5c5ce194f8f892a767bae7a5130ec8f6b8e
bjamyl <alhassanjamil0@gmail.com>
2022-10-25 22:43:04 +0000

    added new model fields

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from email.policy import default
+from xmlrpc.client import Boolean
 from django.db import models
+from django.forms import BooleanField
 
 # Create your models here.
 class Product(models.Model):
@@ -8,6 +10,9 @@ class Product(models.Model):
     price = models.IntegerField()
     image = models.ImageField(upload_to='photos/%Y/%m/%d/')
     in_stock = models.BooleanField(default=True)
+    is_featured_product = models.BooleanField(default=False)
+    is_best_seller = models.BooleanField(default=False)
+    category = models.CharField(max_length=200,null=True)
     def __str__(self):
         return self.name
     
