5287517fa4bb9f30b37477632ba4cc7219d1f223
bjamyl <alhassanjamil0@gmail.com>
2022-12-15 04:19:50 +0000

    minor change for deployment

--- products/api/urls.py
+++ products/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import ProductsView, ProductDetail,SingleProduct
 
 
+
 urlpatterns = [
     path('products/', ProductsView.as_view(), name='all-products'),
     # path('products/<int:pk>', ProductDetail.as_view(), name='product-detail'),
